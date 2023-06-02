f651194e8d64bd8c139300d68c03531bf621d589
Daniil Istomin <istomin.05.3@gmail.com>
2023-06-02 22:39:04 +0700

    #12 Add error tracking

--- src/o_rate/main.c
+++ src/o_rate/main.c
@@ -6,7 +6,7 @@
 int main() {
   int mode;
   char filename[25];
-  char *error_msg = malloc(sizeof(char) * 100);
+  char *error_msg = malloc(sizeof(char) * 150);
   printf("Данная программа поможет вам подобрать оптимальный тариф, исходя "
          "из ваших расходов по пакетам.\nВыберите режим подбора:\n\n1 - "
          "Подбор осуществляется в соответствии с максимальными расходами\n2 "
@@ -18,9 +18,19 @@ int main() {
          "расходах: ");
   scanf("%s", filename);
   struct Expense expense = get_expense_from_file(filename, mode, error_msg);
+  if (expense.minutes < 0) {
+    printf("\nОшибка: %s\n", error_msg);
+    free(error_msg);
+    return -1;
+  }
   printf("\nВаши расходы:\nМинуты: %d, гигабайты: %d, сообщения: %d\n\n",
          expense.minutes, expense.internet, expense.sms);
   struct Rate optimal_rate = find_optimal_rate(expense, error_msg);
+  if (optimal_rate.minutes < 0) {
+    printf("\nОшибка: %s\n", error_msg);
+    free(error_msg);
+    return -1;
+  }
   if (optimal_rate.minutes != 2147483647)
     printf("Был подобран оптимальный тариф %s за %dр.\nВ него "
            "входит\nМинуты: %d, гигабайты: %d, сообщения: %d\n",
@@ -31,6 +41,6 @@ int main() {
            "входит\nМинуты: безлимит, гигабайты: %d, сообщения: %d\n",
            optimal_rate.name, optimal_rate.price, optimal_rate.internet,
            optimal_rate.sms);
-
+  free(error_msg);
   return 0;
 }
