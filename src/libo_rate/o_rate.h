b5dd86c01daff6a65b66c1928748ccd16fb9c4b0
Daniil Istomin <istomin.05.3@gmail.com>
2023-06-03 15:51:22 +0700

    #14 Add searchFile declaration

--- src/libo_rate/o_rate.h
+++ src/libo_rate/o_rate.h
@@ -19,3 +19,5 @@ struct Expense get_expense_from_file(char *filename, int average,
                                      char *error_msg);
 
 struct Rate find_optimal_rate(struct Expense expense, char *error_msg);
+
+int searchFile(char *filename, const char *reldir);
