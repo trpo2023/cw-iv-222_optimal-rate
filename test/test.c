4c6775afa15f01acbd1c3f7b4c0ded80e9f189d9
sergeyzhelezkin <sergeyzhelezkin@users.noreply.github.com>
2023-06-03 15:16:15 +0700

    #14 Add first test in test.c

---
+++ test/test.c
@@ -0,0 +1,17 @@
+#include <dirent.h>
+#include <ctest.h>
+#include <ctype.h>
+#include <string.h>
+
+#include <errors.h>
+#include <o_rate.h>
+
+CTEST (searchFile, test_1)
+{
+    char filename[100] = "expenses.json";
+    const char reldir[] = "../thirdparty/";
+    int result =(searchFile(filename, reldir));
+
+          ASSERT_EQUAL(0, result);
+          ASSERT_STR("../thirdparty/expenses.json", filename);
+}
