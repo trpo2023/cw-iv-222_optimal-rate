48ccd04643bc297ede28eb563a17d810c94bcf6a
Daniil Istomin <istomin.05.3@gmail.com>
2023-06-03 15:52:19 +0700

    #14 Fix test.c

--- test/test.c
+++ test/test.c
@@ -3,14 +3,13 @@
 #include <ctype.h>
 #include <string.h>
 
-#include <errors.h>
 #include <o_rate.h>
 
 CTEST (searchFile, test_1)
 {
     char filename[100] = "expenses.json";
     const char reldir[] = "../thirdparty/";
-    int result =(searchFile(filename, reldir));
+    int result = searchFile(filename, reldir);
 
           ASSERT_EQUAL(0, result);
           ASSERT_STR("../thirdparty/expenses.json", filename);
