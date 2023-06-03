df8b99aed2d0e9d2aad307b2eb64a04774202a07
Daniil Istomin <istomin.05.3@gmail.com>
2023-06-03 15:53:01 +0700

    #14 Fix codestyle

--- test/test.c
+++ test/test.c
@@ -1,16 +1,15 @@
-#include <dirent.h>
 #include <ctest.h>
 #include <ctype.h>
+#include <dirent.h>
 #include <string.h>
 
 #include <o_rate.h>
 
-CTEST (searchFile, test_1)
-{
-    char filename[100] = "expenses.json";
-    const char reldir[] = "../thirdparty/";
-    int result = searchFile(filename, reldir);
+CTEST(searchFile, test_1) {
+  char filename[100] = "expenses.json";
+  const char reldir[] = "../thirdparty/";
+  int result = searchFile(filename, reldir);
 
-          ASSERT_EQUAL(0, result);
-          ASSERT_STR("../thirdparty/expenses.json", filename);
+  ASSERT_EQUAL(0, result);
+  ASSERT_STR("../thirdparty/expenses.json", filename);
 }
